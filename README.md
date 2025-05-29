fff7437e8f7213581993bf85ee619fc587812507
jan <janailson100@gmail.com>
2025-05-29 20:15:09 -0300

    Ajuste

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 
 Janailson Rocha de Sousa (janailson.sousa@aluno.faculdadeimpacta.com.br)<br>
 Deivid Da Silva Florecio (deivid.florencio@aluno.faculdadeimpacta.com)<br>
-Rodrigo ()<br>
-
+Rodrigo Assimos (rodrigo.assimos@aluno.faculdadeimpacta.com.br)<br> 
+Lucas da Silva Braga Cassidori(lucas.cassidori@aluno.faculdadeimpacta.com.br)<br>
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -23,13 +23,13 @@ export class UsersService {
 
   findOne(id: number): User {
     const user = this.users.find((u) => u.id === id);
-    if (!user) throw new NotFoundException('User not found');
+    if (!user) throw new NotFoundException('usuário não encontrado');
     return user;
   }
 
   remove(id: number): void {
     const index = this.users.findIndex((u) => u.id === id);
-    if (index === -1) throw new NotFoundException('User not found');
+    if (index === -1) throw new NotFoundException('usuário não encontrado');
     this.users.splice(index, 1);
   }
 }
